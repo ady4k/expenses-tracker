e340f820de7cd9a46f2ced09c45417a1d302a901
Adrian V <visla.adrian@gmail.com>
2023-04-26 18:09:42 +0300

    Added initial custom TextField files

---
+++ Expenses Tracker/CustomViews/NumericOnlyTextField.swift
@@ -0,0 +1,20 @@
+//
+//  NumericOnlyTextField.swift
+//  Expenses Tracker
+//
+//  Created by Adrian Vîslă on 26.04.2023.
+//
+
+import UIKit
+
+class NumericOnlyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
